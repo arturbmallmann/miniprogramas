ff9338fd18f41cca7eee55e837c8ff86e00d35cb
arturbmallmann <arturbmallmann@gmail.com>
2018-08-24 09:41:54 -0300

    lol

--- scripts/brilhoTela.py
+++ scripts/brilhoTela.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Author: Artur Bernardo Mallmann
-# License: Make what you want, or pay me a beer!!!
-# Version: 0.000.000.r1alphace
+# Mail: arturbmallmann@gmail.com
+# License: Make what you want, and if you want pay me a beer!!!
+# Version: 0.000.000.r2alphace
 import sys
 import math
-args=sys.argv
 #print (args[1:3]) listas como usar: https://youtu.be/W8KRzm-HUcc
 class MyException(LookupError):
     '"my exception"'
@@ -12,7 +12,7 @@ class MyException(LookupError):
 class File():
     def __init__(self, path):
         try:
-            self.file=open(path,'r+')
+            self.file=open(path)
         except:
             raise MyException();
     def read(self):
@@ -23,46 +23,53 @@ class File():
         self.file.seek(0)
         self.file.write(str(value)+'\n')
         self.file.flush()
+
+class Bright(File):
+    def __init__(self,path,maxvalue):
+        self.file=open(path,'+r')
+        self.maxvalue=maxvalue
     def increase(self):
         value=math.log(self.read())
         value+=.25
-        if math.exp(value)>=1500:
-            value=math.log(1500)
+        if math.exp(value)>=self.maxvalue:
+            value=math.log(self.maxvalue)
         print (value)
         self.write(int(math.exp(value)))
     def decrease(self):
         value=math.log(self.read())
         value-=.25
-        if math.exp(value)<5:
+        if value<=2:
             return
         print(value)
         self.write(int(math.exp(value)))
-def getBright(filename):
-    try:
-        file=open(filename)
-        num=int(file.readline())
-        file.close()
-        return num
-    except:
-        raise MyException();
+
+#def getBright(filename):
+#    try:
+#        file=open(filename)
+#        num=int(file.readline())
+#        file.close()
+#        return num
+#    except:
+#        raise MyException();
 
     
+args=sys.argv
 def main():
     filename="/sys/class/backlight/intel_backlight/brightness"
+    filemax="/sys/class/backlight/intel_backlight/max_brightness"
 #    filename="/home/artur/teste"
 #value=getBright(file)
-    print (filename)
-    myFile=File(filename)
+    maxfile=File(filemax)
+    maxvalue=maxfile.read();
+    myFile=Bright(filename,maxvalue)
     
-    value=myFile.read();
-    print (value)
-
-
     for item in args:
         if item=='up':
             myFile.increase()
         elif item=='down':
             myFile.decrease()
+        elif item=='read':
+            print(myFile.read())
             
 if __name__=='__main__':
     main()
