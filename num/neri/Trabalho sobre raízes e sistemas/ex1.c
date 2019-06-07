b978edccbe23db4a7f77005fd80c84be263d8b14
arturbmallmann <arturbmallmann@gmail.com>
2019-06-07 11:12:10 -0300

    sync

--- num/neri/Trabalho sobre raízes e sistemas/ex1.c
+++ num/neri/Trabalho sobre raízes e sistemas/ex1.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
+#define D 5
+#define A 7
 int main(){
-    int a=7;
     int mat=1511100032;
-    printf("%d + (%d mod 5)=%d\n",a,mat,a+mat%5);
+    printf("%d + (%d mod %d)=%d\n",A,mat,D,A+mat%D);
 
 } 
